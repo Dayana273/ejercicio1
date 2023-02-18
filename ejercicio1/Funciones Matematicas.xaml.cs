c0d1c2734fc8eb8ef34dd140160e925a3ffc10e5
Dayana <Dayana@DESKTOP-BMET23Q>
2023-02-18 15:35:20 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1/Funciones Matematicas.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ejercicio1.NewFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Funciones_Matematicas : ContentPage
+    {
+        public object Number1Entry { get; private set; }
+
+        public Funciones_Matematicas()
+        {
+            InitializeComponent();
+        }
+
+        private void txtcalcular_Clicked(object sender, EventArgs e)
+        {
+
+            var solu = new solucion
+            {
+               
+
+
+
+
+
+            };
+
+        }
+
+
+
+        private void txtresta_Clicked(object sender, EventArgs e)
+
+        {
+           
+            
+                
+        
+            
+
+        }
+
+        private void txtdividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtsuma_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtmultiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
